9b3632094b27fb5ae1f71a8fd2cbacecc24d7700
001232957 <Braydon.NUGENT@student.tafesa.edu.au>
2025-09-13 12:54:45 +0930

    Decreased all ticket prices by $10

--- Practical Test Preparation/Student Files/Ticketing/Ticketing/TicketPrice.cs
+++ Practical Test Preparation/Student Files/Ticketing/Ticketing/TicketPrice.cs
@@ -14,9 +14,9 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 40.5m;
-        const decimal mdecGeneral = 33.75m;
-        const decimal mdecBox = 67.0m;
+        const decimal mdecBalcony = 30.5m;
+        const decimal mdecGeneral = 23.75m;
+        const decimal mdecBox = 57.0m;
         const decimal mdecDiscount = 10.0m; 
 
         private int Section
